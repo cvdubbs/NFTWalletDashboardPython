afbe280f5db808e1b5c21e9d8c67887fe29aa8a9
Christopher VanWert <christophervanwert@gmail.com>
2023-02-09 22:48:14 -0500

    added try except for get os volume

--- NFTLib.py
+++ NFTLib.py
@@ -5,64 +5,6 @@ from pandas import json_normalize
 import pandas as pd
 import numpy as np
 
-# from moralis import evm_api
-
-# def NFTs_owned(walletaddress):
-#     """
-#     Get NFTs owned by walletaddress
-
-#     Parameters
-#     ----------
-#     walletaddress : str
-#         Ethereum Mainnet Address
-
-
-#     Returns
-#     -------
-#     dataframe
-#         Name, ContractID, Quantity
-#     """
-#     api_key = "xeulBfSFsvTH7mW2tJLrBY6J4YdtrKJr7of9ydrUhqnmcGzajyMdZa8Np3HGF09K"
-#     params = {
-#         "address": walletaddress, 
-#         "chain": "eth", 
-#         "format": "decimal", 
-#         "limit": 100, 
-#         "token_addresses": [], 
-#         "cursor": "", 
-#         "normalizeMetadata": True, 
-#     }
-#     result = evm_api.nft.get_wallet_nfts(
-#         api_key=api_key,
-#         params=params,
-#     )
-#     result_df = pd.DataFrame.from_dict(result['result'])
-
-#     while (result['cursor'] != None):
-#         params = {
-#         "address": walletaddress, 
-#         "chain": "eth", 
-#         "format": "decimal", 
-#         "limit": 100, 
-#         "token_addresses": [], 
-#         "cursor": result['cursor'], 
-#         "normalizeMetadata": True, 
-#         }
-#         result = evm_api.nft.get_wallet_nfts(
-#             api_key=api_key,
-#             params=params,
-#         )
-#         result_add_df = pd.DataFrame.from_dict(result['result'])
-#         frames = [result_df, result_add_df]
-#         result_df = pd.concat(frames)
-
-#     result_df = result_df[['name','token_address']]
-#     result_df = result_df.groupby(result_df.columns.tolist(), as_index=False).size()
-#     # result_df['urlname'] = result_df['name'].str.lower()
-#     # result_df['urlname'] = result_df['urlname'].str.replace(' ','-')
-#     return result_df
-
-
 def get_os_floor(contractid):
     """
     Get NFT Floor by collection contract
@@ -123,6 +65,8 @@ def NFTs_owned_alchemy(walletaddress):
             df['token_address'].loc[i] = (pain2_dict['address'])
     df = df[['name', 'token_address']]
     df = df.groupby(df.columns.tolist(), as_index=False).size()
+    df = df.reset_index()
+    df = df.drop(columns='index')
     return df
 
 def get_os_vol(contractAddy):
@@ -140,7 +84,10 @@ def get_os_vol(contractAddy):
     else:
         fp_df = df.loc[df.index == 'floor_price']
         tv_df = df.loc[df.index == 'total_volume']
-        return tv_df.iloc[0,1]
+        try:
+            return tv_df.iloc[0,1]
+        except:
+            return 0
 
 def GetCombine(walletaddress):
     """
@@ -166,89 +113,3 @@ def GetCombine(walletaddress):
     nfts_owned_ed.loc[ nfts_owned_ed["floor_price"] == "unable to fetch floor price", "floor_price"] = 0
     nfts_owned_ed["total"] = nfts_owned_ed['floor_price'] * nfts_owned_ed['size']
     return nfts_owned_ed
-
-# alchemy_url = "https://eth-mainnet.g.alchemy.com/v2/DDEUdTIi3FXhHcYRDTff8jVeSiz3dZwC"
-# w3 = Web3(Web3.HTTPProvider(alchemy_url))
-# walletaddress = '0x5e9c948A7197D4c83D71dC869b4f0Eb968cafC7a'
-# url = "https://eth-mainnet.g.alchemy.com/nft/v2/DDEUdTIi3FXhHcYRDTff8jVeSiz3dZwC/getNFTs?owner={}&pageSize=100&withMetadata=true".format(walletaddress)
-# headers = {"accept": "application/json"}
-# response = requests.get(url, headers=headers)
-# data = response.text
-# dicty = json.loads(data)
-# df = pd.DataFrame.from_dict(dicty['ownedNfts'])
-# return df
-
-
-# ***** THIS IS ALL TESTING BELOW *****
-# walletaddress = '0x5e9c948A7197D4c83D71dC869b4f0Eb968cafC7a'
-# api_key = "xeulBfSFsvTH7mW2tJLrBY6J4YdtrKJr7of9ydrUhqnmcGzajyMdZa8Np3HGF09K"
-# params = {
-#     "address": walletaddress, 
-#     "chain": "eth", 
-#     "format": "decimal", 
-#     "limit": 100, 
-#     "token_addresses": [], 
-#     "cursor": "", 
-#     "normalizeMetadata": True, 
-# }
-# result = evm_api.nft.get_wallet_nfts(
-#     api_key=api_key,
-#     params=params,
-# )
-# result_df = pd.DataFrame.from_dict(result['result'])
-
-# while (result['cursor'] != None):
-#     params = {
-#     "address": walletaddress, 
-#     "chain": "eth", 
-#     "format": "decimal", 
-#     "limit": 100, 
-#     "token_addresses": [], 
-#     "cursor": result['cursor'], 
-#     "normalizeMetadata": True, 
-#     }
-#     result = evm_api.nft.get_wallet_nfts(
-#         api_key=api_key,
-#         params=params,
-#     )
-#     result_add_df = pd.DataFrame.from_dict(result['result'])
-#     frames = [result_df, result_add_df]
-#     result_df = pd.concat(frames)
-
-# #cleanup df
-# result_df = result_df[['name','token_address']]
-# result_df = result_df.groupby(result_df.columns.tolist(), as_index=False).size()
-# result_df['urlname'] = result_df['name'].str.lower()
-# result_df['urlname'] = result_df['urlname'].str.replace(' ','-')
-# return result_df
-
-# While result['cursor'] != "":
-#   get result again but with cursor = result['cursor'
-# walletaddress = '0xE74dFE8d4dbd080f6c0cb34A11CFbACBFE315439'
-# print(GetCombine(walletaddress))
-# nfts_owned_ed = NFTs_owned_alchemy(walletaddress)
-# nfts_owned_ed["floor_price"] = np.nan
-# nfts_owned_ed["total_volume"] = np.nan
-
-# tdf = NFTs_owned(walletaddress)
-# tdf["floor_price"] = np.nan
-# tdf["total_volume"] = np.nan
-
-
-# walletaddress = '0x5e9c948A7197D4c83D71dC869b4f0Eb968cafC7a'
-# nfts_owned_ed = NFTs_owned(walletaddress)
-# nfts_owned_ed2 = NFTs_owned_alchemy(walletaddress)
-
-
-# Get from Alchemy and compare to moralis - needs cursor or resender
-
-#testing loop
-# pk_next = dicty['pageKey']
-# url = "https://eth-mainnet.g.alchemy.com/nft/v2/DDEUdTIi3FXhHcYRDTff8jVeSiz3dZwC/getNFTs?owner={}&pageKey={}&pageSize=100&withMetadata=true".format(walletaddress, pk_next)
-# response = requests.get(url, headers=headers)
-# data = response.text
-# dicty = json.loads(data)
-# df_add = pd.DataFrame.from_dict(dicty['ownedNfts']) 
-# frames = [df, df_add]
-# df = pd.concat(frames)
-# print(dicty['pageKey'])
